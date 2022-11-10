636df5a808675e8ee98028b46aabc10fdfc629fd
reshad21 <miakala7@gmail.com>
2022-11-10 20:18:23 +0600

    update query execute

--- index.js
+++ index.js
@@ -25,34 +25,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        const UserCollection = client.db("winterShopDb").collection("registeruser");
-
-        // send dat to the database site
-        app.post('/profile', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const result = await UserCollection.insertOne(user);
-            res.send(result);
-        })
-
-        // now we take all the register data from database
-        app.get('/profile', async (req, res) => {
-            let query = {};
-            const cursor = UserCollection.find(query);
-            const users = await cursor.toArray();
-            console.log(users);
-            res.send(users);
-        })
-
-
-        // delete a single user from database
-        app.delete('/userdlt/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) }
-            const user = await UserCollection.deleteOne(query);
-            res.send(user);
-        })
-
 
         // ========= wildanimal project ========= //
         const AddServiceCollection = client.db("winterShopDb").collection("services");
@@ -109,6 +81,25 @@ async function run() {
             res.send(users);
         })
 
+        // get data by email adress
+        app.get('/emailuserreview', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = userReviewCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+
+        // ==================================== //
+
+        // ==================================== //
+
+
         // get single review for a user
         app.get('/userreview/:id', async (req, res) => {
             const id = req.params.id;
@@ -117,23 +108,40 @@ async function run() {
             res.send(user);
         })
 
-        // app.get('/userreview', async (req, res) => {
-        //     let query = {};
-        //     if (req.query.email) {
-        //         query = {
-        //             email: req.query.email
-        //         }
-        //     }
-        //     const cursor = userReviewCollection.find(query);
-        //     const orders = await cursor.toArray();
-        //     res.send(orders);
-        // })
-
         // Delete a single review from client side
         app.delete('/userreview/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await userReviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        // Update data from data base //
+        // step:1 take single data from api
+        app.get('/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const user = await userReviewCollection.findOne(query);
+            res.send(user);
+        })
+
+        // step:2 now we update the value
+        app.put('/update/:id', async (req, res) => {
+            const id = req.body.params;
+            const filter = { _id: ObjectId(id) }
+            const user = req.body;
+            const option = { upsert: true };
+            const updatedUser = {
+                $set: {
+                    // name: user.name,
+                    // address: user.address,
+                    // email: user.email,
+                    review: user.review,
+
+                }
+            }
+            const result = await userReviewCollection.updateOne(filter, updatedUser, option);
             res.send(result);
         })
 
